512ef812887c8fdffe86a9eac92f9a06b92576a7
Imtius10 <h.imtius10@gmail.com>
2026-06-09 03:12:07 +0600

    update issue table

--- src/DB/index.ts
+++ src/DB/index.ts
@@ -4,29 +4,30 @@ import config from "../Config/config.index";
 export const sql = neon(config.db_url);
 
 export const initDB = async () => {
-  await sql`
-    CREATE TABLE IF NOT EXISTS users(
+ await sql`
+    CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
         name VARCHAR(75) NOT NULL,
         email VARCHAR(255) UNIQUE NOT NULL,
         password_hash TEXT NOT NULL,
-        role VARCHAR(20) NOT NULL DEFAULT 'user',
-        created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-        updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+        role VARCHAR(20) NOT NULL DEFAULT 'contributor' CHECK (role IN ('contributor', 'maintainer')),
+        created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+        updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
     )
-    `;
+  `;
 
-  await sql`
+ // 2. Updated ISSUES Table
+ await sql`
     CREATE TABLE IF NOT EXISTS issues (
         id SERIAL PRIMARY KEY,
         title VARCHAR(150) NOT NULL,
         description TEXT NOT NULL CHECK (LENGTH(description) >= 20),
-        type TEXT NOT NULL,
-        status TEXT NOT NULL DEFAULT 'open',
-        reporter_id INT NOT NULL, -- Application logic will validate existence against users(id)
-        created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-        updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+        type VARCHAR(20) NOT NULL CHECK (type IN ('bug', 'feature_request')),
+        status VARCHAR(20) NOT NULL DEFAULT 'open' CHECK (status IN ('open', 'in_progress', 'resolved')),
+        reporter_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+        created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+        updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
     )
-    `;
+  `;
   console.log("db conn");
 };
--- src/type/type.ts
+++ src/type/type.ts
@@ -1,9 +1,14 @@
 export const UserRole = ["contributor", "maintainer"] as const;
+export type IssueType = "bug" | "feature_request";
+export type IssueStatus = "open" | "in_progress" | "resolved";
+type Role = (typeof UserRole)[number];
 
-type Role = typeof UserRole[number];
-
-
- export type User = {
+export interface IGetIssuesFilters {
+  sort: "newest" | "oldest";
+  type?: IssueType | undefined; 
+  status?: IssueStatus | undefined; 
+}
+export type User = {
   id: number;
   name: string;
   email: string;
@@ -12,5 +17,33 @@ type Role = typeof UserRole[number];
   created_at: Date;
   updated_at: Date;
 };
-export type RUser = Omit<User, "id" | "password_hash" | "created_at" | "updated_at">;
+export type RUser = Omit<
+  User,
+  "id" | "password_hash" | "created_at" | "updated_at"
+>;
+
+export interface Issue {
+  id: number;
+  title: string;
+  description: string;
+  type: IssueType;
+  status: IssueStatus;
+  reporter_id: number;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface IssueWithReporter extends Omit<Issue, "reporter_id"> {
+  reporter: {
+    id: number;
+    name: string;
+    role: Role;
+  };
+}
+
+export interface JwtPayload {
+  id: number;
+  name: string;
+  role: Role;
+}
 
