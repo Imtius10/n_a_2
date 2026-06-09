1d9e821b001601883132c266860ccfb292018c43
Imtius10 <h.imtius10@gmail.com>
2026-06-09 17:50:36 +0600

    added issue controller

--- src/API/Controller/Issue.Controller.ts
+++ src/API/Controller/Issue.Controller.ts
@@ -2,14 +2,71 @@ import type { Request, Response } from "express";
 import IssueService from "../Service/Issue.service";
 import { sendError, sendSuccess } from "../../GlobalError/SuccessAndError";
 
-
-
 export const createIssue = async (req: Request, res: Response) => {
-    try {
-        const { title, description, type } = req.body
-        const issue = await IssueService.createIssue(title, description, type, req.user.id);
-        return sendSuccess(res, 201, "Issue created successfully", issue);
-    } catch (error) {
-         return sendError(res, 500, "Internal Server Error", error);
+  try {
+    const { title, description, type } = req.body;
+    const issue = await IssueService.createIssue(
+      title,
+      description,
+      type,
+      req.user.id,
+    );
+    return sendSuccess(res, 201, "Issue created successfully", issue);
+  } catch (error) {
+    return sendError(res, 500, "Internal Server Error", error);
+  }
+};
+
+export const getIssueById = async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.params.id);
+
+    const issue = await IssueService.getIssueById(id);
+
+    if (!issue) {
+      return sendError(res, 404, "Issue not found");
     }
-}
+
+    return sendSuccess(res, 200, "Issue retrieved successfully", issue);
+  } catch (error) {
+    return sendError(res, 500, "Internal Server Error", error);
+  }
+};
+
+export const updateIssue = async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.params.id);
+    const { title, description, type } = req.body;
+
+    const updated = await IssueService.updateIssue(
+      id,
+      title,
+      description,
+      type,
+    );
+
+    if (!updated) {
+      return sendError(res, 404, "Issue not found");
+    }
+
+    return sendSuccess(res, 200, "Issue updated successfully", updated);
+  } catch (error) {
+    return sendError(res, 500, "Internal Server Error", error);
+  }
+};
+
+export const deleteIssue = async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.params.id);
+
+    const deleted = await IssueService.deleteIssue(id);
+
+    if (!deleted) {
+      return sendError(res, 404, "Issue not found");
+    }
+
+    return sendSuccess(res, 200, "Issue deleted successfully", deleted);
+  } catch (error) {
+    return sendError(res, 500, "Internal Server Error", error);
+  }
+};
--- src/API/Routes/issue.routes.ts
+++ src/API/Routes/issue.routes.ts
@@ -1,11 +1,28 @@
 import { Router } from "express";
 import { auth, authorizeRole } from "../../Middleware/auth";
-import { createIssue } from "../Controller/Issue.Controller";
+import {
+  createIssue,
+  deleteIssue,
+  getIssueById,
+  updateIssue,
+} from "../Controller/Issue.Controller";
 
 export const router = Router();
 
+router.use(
+  "/createIssue",
+  auth,
+  authorizeRole("contributor", "maintainer"),
+  createIssue,
+);
+//router.patch("/:id/status", auth, authorizeRole("maintainer"), updateIssue);
+router.get("/issue/:id", getIssueById);
 
-router.use("/createIssue",auth ,authorizeRole("contributor", "maintainer"),createIssue)
-
-
-export const IssueRoute = router
+router.patch(
+  "/issue/:id",
+  auth,
+  authorizeRole("contributor", "maintainer"),
+  updateIssue,
+);
+router.delete("/issue/:id", auth, authorizeRole("maintainer"), deleteIssue);
+export const IssueRoute = router;
